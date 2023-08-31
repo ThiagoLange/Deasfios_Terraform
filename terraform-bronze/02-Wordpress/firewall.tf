58abdf8e1a2ff1a02246f410f83873449f777df8
Thiago Egon Lange <thiago_lange@yahoo.com.br>
2023-08-31 18:20:05 -0300

    Update the correct ips in teh firewall

--- terraform-bronze/02-Wordpress/firewall.tf
+++ terraform-bronze/02-Wordpress/firewall.tf
@@ -5,31 +5,31 @@ resource "digitalocean_firewall" "firewall" {
   inbound_rule {
     protocol         = "tcp"
     port_range       = "22"
-    source_addresses = ["239.13.117.114/32", "::/0"]
+    source_addresses = ["239.13.117.114/32"]
   }
 
   inbound_rule {
     protocol         = "tcp"
     port_range       = "53"
-    source_addresses = ["0.0.0.0/0", "::/0"]
+    source_addresses = ["239.13.117.114/32"]
   }
 
   inbound_rule {
     protocol         = "tcp"
     port_range       = "443"
-    source_addresses = ["0.0.0.0/0", "::/0"]
+    source_addresses = ["239.13.117.114/32"]
   }
 
   inbound_rule {
     protocol         = "tcp"
     port_range       = "80"
-    source_addresses = ["0.0.0.0/0", "::/0"]
+    source_addresses = ["239.13.117.114/32"]
   }
 
   outbound_rule {
     protocol              = "tcp"
     port_range            = "22"
-    destination_addresses = ["239.13.117.114/32", "::/0"]
+    destination_addresses = ["0.0.0.0/0", "::/0"]
   }
 
   outbound_rule {
