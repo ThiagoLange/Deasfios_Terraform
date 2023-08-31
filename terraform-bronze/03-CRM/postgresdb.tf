fab434d45cfcaf9459ec439aadc772eac34e04c9
Thiago Egon Lange <thiago_lange@yahoo.com.br>
2023-08-31 15:06:05 -0300

    Add outputs and db resource

---
+++ terraform-bronze/03-CRM/postgresdb.tf
@@ -0,0 +1,17 @@
+resource "digitalocean_database_cluster" "postgres-crm" {
+  name       = "postgres-cluster-crm"
+  engine     = var.engine
+  version    = "15"
+  size       = var.size_db
+  region     = var.region
+  node_count = 1
+}
+
+resource "digitalocean_database_firewall" "database-firewall" {
+  cluster_id = digitalocean_database_cluster.postgres-crm.id
+
+  rule {
+    type  = "droplet"
+    value = digitalocean_droplet.vm_cn.id
+  }
+}
