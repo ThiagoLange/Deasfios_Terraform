eac9b90020cade06fcabd7309a3d5ec42704170d
Thiago Egon Lange <thiago_lange@yahoo.com.br>
2023-09-05 17:36:35 -0300

    Add count to create 3 control planes

--- terraform-prata/02-kubernetes-ambiente-ha/droplet.tf
+++ terraform-prata/02-kubernetes-ambiente-ha/droplet.tf
@@ -1,10 +1,11 @@
-resource "digitalocean_droplet" "control_plane" {
+resource "digitalocean_droplet" "control-plane" {
   image    = var.image
-  name     = "control-plane"
+  name     = "${var.droplet_name}-${count.index + 1}"
   region   = var.region
   size     = var.size
   ssh_keys = [digitalocean_ssh_key.ssh.fingerprint]
   vpc_uuid = digitalocean_vpc.vpc-k8s.id
+  count    = var.node_count
 }
 
 resource "digitalocean_droplet" "worker-node" {
